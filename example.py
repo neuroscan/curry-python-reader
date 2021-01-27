fe4103f60e9aa6fbb7448c08bcbc7ff1f0cd1cf0
Fernando Gasca <fgasca@neuroscan.com>
2021-01-27 14:12:22 +0100

    Uncommented line

--- example.py
+++ example.py
@@ -4,8 +4,8 @@ import curryreader as cr
 #(data, datainfo, labels, events, annotations, sensorpos, impedancematrix, hpimatrix) = cr.read()
 
 # 2) Specify file path, avoid plot, set minimum verbosity, get all outputs in tuple "data":
-#data = cr.read("test_data\\Float.cdt", 0, 1)
+data = cr.read("test_data\\Float.cdt", 0, 1)
 
 # 3) Get only first x outputs:
-data, datainfo, labels, *_ = cr.read("test_data\\Float.cdt")
-print("Info (samples, channels, trials/epochs, sampling frequency):\n", datainfo) 
+#data, datainfo, labels, *_ = cr.read("test_data\\Float.cdt")
+#print("Info (samples, channels, trials/epochs, sampling frequency):\n", datainfo) 
